f3f9a63110f292a04272bb4e5f73df26a01ce40d
lotfio <contact@lotfio.net>
2020-01-07 21:48:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center">
+  <img src="https://github.com/lotfio/drag-upload/blob/master/docs/example.png" alt="drag upload Preview">
+    <strong>js drag and preview for image upload</strong>
+  </p>
+</p>
+
+### :fire: Introduction :
+js drag and preview for image upload
---
+++ assets/js/drag.js
@@ -0,0 +1,61 @@
+'use strict';
+// helpers
+let __ = (id) => document.getElementById(id);
+
+// disable drag and drop on window
+window.addEventListener('dragover', (e) => { e.preventDefault();});
+window.addEventListener('drop', (e) => { e.preventDefault();});
+
+// set place holder and preview
+let setPlaceholderAndPreview = (files) => {
+    // check if image
+    let allowed = ['png', 'jpg', 'jpeg', 'gif'];
+    
+    let ext = files[0].name.split('.');
+        ext = ext[ext.length - 1];
+
+        // if not allowed 
+        if(allowed.indexOf(ext) == '-1')
+        {
+            __('file-info').style.display    = 'none';
+            __('preview-icon').style.display = 'none';
+            __('file-error').style.display   = 'block';
+            __('file-error').lastElementChild.innerHTML = "File type not allowed <br> only " + allowed.join(',') + " are allowed";
+            
+            return false;
+        }
+
+     // set files 
+    __('file').files                 = files;
+    
+    // display file info
+    __('file-info').style.display  = 'block';
+    __('file-error').style.display = 'none';
+    // set place holder
+    __('file-placeholder').innerHTML = files[0].name;
+    // display preview icon
+    __('preview-icon').style.display = 'block';
+    // add image to preview
+    let imageUrl = URL.createObjectURL(files[0]);
+    __('preview-box').firstElementChild.src = imageUrl;
+};
+
+// on drop
+__('upload-area').addEventListener('drop', (e) =>{
+    setPlaceholderAndPreview(e.dataTransfer.files);
+});
+
+// on click 
+__('file').addEventListener('change', (e) => {
+    setPlaceholderAndPreview(e.target.files);
+});
+
+// on hover display preview
+__('preview-icon').addEventListener('mouseover', (e)=>{
+    __('preview-box').style.display = 'block';
+});
+
+// on hover display preview
+__('preview-icon').addEventListener('mouseout', (e)=>{
+    __('preview-box').style.display = 'none';
+});
